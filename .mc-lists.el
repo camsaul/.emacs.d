af4400ba2b5b842c277be4d1f38b37d127795533
Cam Saul <cam@geotip.com>
2015-05-26 20:30:42 -0700

    add some more things to mc-lists :sob:

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,13 +3,45 @@
 
 (setq mc/cmds-to-run-for-all
       '(
-	paredit-backward
-	paredit-forward
-	paredit-forward-delete
-	paredit-open-square
-	))
+        backward-sexp
+        cam/join-next-line
+        clojure-toggle-keyword-string
+        electric-newline-and-maybe-indent
+        electric-pair-delete-pair
+        forward-sexp
+        indent-for-tab-command
+        insert-register
+        join-next-line
+        kill-region
+        kill-sexp
+        kmacro-end-or-call-macro
+        paredit-backslash
+        paredit-backward
+        paredit-close-curly
+        paredit-close-round
+        paredit-close-square
+        paredit-doublequote
+        paredit-forward
+        paredit-forward-delete
+        paredit-forward-slurp-sexp
+        paredit-kill
+        paredit-newline
+        paredit-open-curly
+        paredit-open-round
+        paredit-open-square
+        yaml-electric-backspace
+        ))
 
 (setq mc/cmds-to-run-once
       '(
-	mc/mark-next-lines
-	))
+        ace-jump-done
+        ace-jump-mode
+        beginning-of-buffer
+        end-of-buffer
+        helm-M-x
+        jump-to-register
+        mc/mark-next-lines
+        next-buffer
+        paredit-mode
+        previous-buffer
+        ))
