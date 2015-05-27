9dd44f2f534a5cc4e0fae0dd4149982bc25b95f7
Cam Saul <cam@geotip.com>
2015-05-26 20:01:40 -0700

    actually just use aggressive-indent-mode in web-mode

--- init.el
+++ init.el
@@ -546,15 +546,10 @@
 
 ;;; [[<Web Mode]]
 (defun cam/web-mode-setup ()
+  (aggressive-indent-mode 1)
   (define-key web-mode-map
     (kbd "C-j") #'newline))                       ; instead of electric-newline-and-maybe-indent which doesn't indent :/
 (add-hook 'web-mode-hook #'cam/web-mode-setup)
-
-;;; after inserting a matching closing tag, fix its indentation
-(eval-after-load 'web-mode
-  '(advice-add #'web-mode-element-close :after (lambda ()
-                                                 (interactive)
-                                                 (call-interactively #'indent-for-tab-command))))
 
 
 ;;; ---------------------------------------- [[<Final Setup]] ----------------------------------------
