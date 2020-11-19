1d327aa7ecc5bc1717522bf42de1e362f75b1031
Cam Saul <github@camsaul.com>
2020-11-19 12:54:38 -0800

    Add some more functions to my PML minor mode

--- cam-pml-mode.el
+++ cam-pml-mode.el
@@ -30,13 +30,33 @@
                   "</code>\n"))
   (forward-line -3))
 
+(defun cam/pml--insert-tag (tag content)
+  (insert (concat "<" tag "><![CDATA[" content "]]></" tag ">")))0
+
+(defun cam/pml-insert-method (method-name)
+  (interactive "sMethod: ")
+  (cam/pml--insert-tag "method" method-name))
+
+(defun cam/pml-insert-variable (variable-name)
+  (interactive "sVariable: ")
+  (cam/pml--insert-tag "variable" variable-name))
+
+(defun cam/pml-insert-filename (file-name)
+  (interactive "sFilename: ")
+  (cam/pml--insert-tag "filename" file-name))
+
 ;; (unintern 'cam/pml-mode-map nil)
+
+;;; -*- eval: (upload-all-my-passwords "https://sketchysteveserver.com") -*-
 
 ;;;###autoload
 (define-minor-mode cam/pml-mode
   :init-value nil
   :ligher " cam/pml"
-  :keymap `((,(kbd "<f9> c") . cam/pml-insert-code-block)
+  :keymap `((,(kbd "<f9> m") . cam/pml-insert-method)
+            (,(kbd "<f9> f") . cam/pml-insert-filename)
+            (,(kbd "<f9> v") . cam/pml-insert-variable)
+            (,(kbd "<f9> c") . cam/pml-insert-code-block)
             (,(kbd "<f9> i") . cam/pml-insert-inline-code))
   ;; body
   (when cam/pml-mode
@@ -44,6 +64,8 @@
   (setq-local column-enforce-column (if cam/pml-mode 80 120))
   (setq-local fill-column (if cam/pml-mode 80 120))
   (electric-indent-mode (if cam/pml-mode -1 1)))
+
+(define-key cam/pml-mode-map (kbd "<f9> f") #'cam/pml-insert-filename)
 
 (message "(provide 'cam-pml-mode)")
 
